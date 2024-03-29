f7767718335ace0b40df59260894febfb818e816
unknown <adam.vaizman@gmail.com>
2024-03-29 15:12:58 +0300

    rm target

--- target/debug/deps/algebrars-5d3a76d908cb46c7.d
+++
@@ -1,5 +0,0 @@
-/home/sickguy/Documents/Projects/algebrars/target/debug/deps/libalgebrars-5d3a76d908cb46c7.rmeta: src/lib.rs
-
-/home/sickguy/Documents/Projects/algebrars/target/debug/deps/algebrars-5d3a76d908cb46c7.d: src/lib.rs
-
-src/lib.rs:
--- target/debug/deps/algebrars-ef229db4fa2c2d19.d
+++
@@ -1,8 +0,0 @@
-/home/sickguy/Documents/Projects/algebrars/target/debug/deps/libalgebrars-ef229db4fa2c2d19.rmeta: src/lib.rs src/number.rs
-
-/home/sickguy/Documents/Projects/algebrars/target/debug/deps/libalgebrars-ef229db4fa2c2d19.rlib: src/lib.rs src/number.rs
-
-/home/sickguy/Documents/Projects/algebrars/target/debug/deps/algebrars-ef229db4fa2c2d19.d: src/lib.rs src/number.rs
-
-src/lib.rs:
-src/number.rs:
